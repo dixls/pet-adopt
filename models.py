ba2e052d5b588c5d539f43ea24df46b4425899cc
pixls <tha.pixls@gmail.com>
2022-03-11 12:50:48 -0500

    setting up database
    added Pet model to models.py with appropriate columns

---
+++ models.py
@@ -0,0 +1,24 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """connect to database"""
+
+    db.app = app
+    db.init_app(app)
+
+
+class Pet(db.Model):
+    """Pet model"""
+
+    __tablename__ = "pets"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50), nullable=False)
+    species = db.Column(db.String(30), nullable=False)
+    photo_url = db.Column(db.String)
+    age = db.Column(db.Integer)
+    notes = db.Column(db.String)
+    available = db.Column(db.Boolean)
